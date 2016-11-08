d71ec641f6e5775362913b0654b509ab197e1578
salvador909 <salvador.mendez94@gmail.com>
2016-11-07 17:55:16 -0800

    updating day 2

---
+++ day 1/js/script.js
@@ -0,0 +1,52 @@
+//console.log("salvador mendez");
+var firstname = "salvador";
+//console.log(firstname);
+//string
+var string1 = "this a string";//string litaral
+var string2 = "i've done this before";// mix and match quotes
+var string3 = string("This is a string also");//string object
+// numbers
+var number1 = 5;//integers 5
+var number2 = "5";//sting 5 not a number
+floats
+var float1  = 2.75;
+var float2  = 245.45678;
+//boolean
+var boolean =true;
+var boolean = false;
+//array
+var array1 = ['bread,eggs,milk'];
+var array2 = ['mat',6.4,false];
+//console.log (array2[1]);// output indez 1 of array
+//object
+var object ={
+  name: 'matt',
+  height: 6.4,
+  caffinated:false
+};
+//console.log(object1.height);
+//create two string variables first name and last name to
+//create two number variables b-day month and bday date
+//create two float variables height and weight
+// create 2 boolean variables gender and pets yes or no
+// create 2 arrays variables 3 fav colors and 3 fav movies
+//create two object variables car (3 iteams) and self (3 iteams)
+//consloe each
+console.log(string)
+var string = "salvador";
+var string = "mendez";
+console.log(number)
+var number = "10";
+var number = "22";
+console.log(float)
+var  float = "5";
+var float = "200";
+console.log(boolean)
+var boolean = "male";
+var boolean = "yes";
+console.log(array)
+var array = "yellow red blue";
+var array = "star wars lotr pokemon ";
+console.log(object)
+var object = "honda toyota ford";
+var object = "hats cloths video games";
---
+++ day 2/script.js
@@ -0,0 +1,61 @@
+//console.log("this is a test message");
+//alert("this is an alert box intruding on your life");
+//confirm box
+//confirm('do you like raisins');
+//console.log(userResp);
+//prompt box
+//var userResp = prompt("what is your quest");
+//var usernum = prompt("what is your fav num");
+//console.log(parseInt(usernum));
+//var username = prompt ("what is your name");
+//var useage = prompt ("what is your age");
+//var usewere = prompt ("where are you");
+//var userconfirm = confirm  ("comfirm place "+ usewere);
+//alert("your name your age " + username + " "+useage);
+
+//var a = 5;
+//var b = 8;
+
+//var sum = a + b;
+//console.log(sum);
+
+//var difference = a - b;
+//console.log(difference);
+
+//var mul = a * b;
+//console.log(mul);
+
+//var div = a / b;
+//console.log(div);
+
+//var mod = a % b;
+//console.log(mod);
+
+//var usernum = parseInt(prompt ("pick a number between 1-100"));
+//console.log(typeof usernum);
+//var userchoice = usernum * 4;
+//console.log(userchoice);
+//var usernewnum = userchoice * 5
+//console.log(usernewnum);
+//var userdiv = usernewnum / usernum;
+//console.log(userdiv);
+//var usersub = userdiv - 7;
+//console.log(usersub);
+//alert (usersub);
+//math object
+//var random = Math.random();
+//console.log(random);
+
+//var roundnum = Math.round(3.390);
+//console.log(rondnum);
+//var floornum = Math.floor(6.390);
+//console.log(floornum);
+//var ceilnum = Math.floor(6.390);
+//console.log(floornum);
+
+var randomnum = Math.random();
+console.log(randomnum);
+randomnum = Math.round(randomnum)
+console.log(randomnum);
+randomnum = randomnum = 100;
+console.log(randomnum);
